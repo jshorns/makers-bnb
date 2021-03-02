dd9eab79a3e863d1a1bc9e7dbf6920236717eac1
Joey <joey.s.hornsby@gmail.com>
2021-03-02 15:56:11 +0000

    implemented user sign up form and success page

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'sinatra/base'
 require './setup_database_connection'
 require './lib/space.rb'
+require './lib/user.rb'
 
 class MakersBNB < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
@@ -11,6 +13,25 @@ class MakersBNB < Sinatra::Base
   get '/spaces' do
     @spaces = Space.all
     erb(:spaces)
+  end
+
+  get '/users/new' do
+    erb(:'users/new')
+  end
+
+  post '/users/new' do
+    user = User.create(
+      name: params[:name],
+      password: params[:password],
+      email: params[:email],
+      username: params[:username]
+    )
+    session[:user] = user
+    redirect '/users/success'
+  end
+
+  get '/users/success' do
+    erb(:'users/success')
   end
 
   run! if app_file == $0
