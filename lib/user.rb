2686f2367303bf5b8f6f042ac06479de27d96e7e
iainaitken <iain_aitken@yahoo.co.uk>
2021-03-02 15:22:45 +0000

    added unit test and feature test for user class/signup, feature test not passing yet

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,13 @@
 require 'pg'
+require 'db_connection'
 
 class User
-  attr_reader :name, :description, :price, :id
+  def self.create(name:, password:, email:, username:)
+    result = DBConnection.query("INSERT INTO users (name, password, email, username) VALUES('#{name}', '#{password}', '#{email}', '#{username}' ) RETURNING id, name, password, email, username;")
+    User.new(id: result[0]['id'], name: result[0]['name'], password: result[0]['password'], email: result[0]['email'], username: result[0]['username'])
+  end
+  
+  attr_reader :name, :email, :username, :id
 
   def initialize(name:, password:, email:, username:, id:)
     @name = name
@@ -10,16 +16,4 @@ class User
     @username = username
     @id = id
   end
-
-  def self.create(name:, password:, email:, username:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'makersbnb_test')
-    else
-      connection = PG.connect(dbname: 'makersbnb')
-    end
-
-    result = connection.exec("INSERT INTO users (name, password, email, username) VALUES('#{name}', '#{password}', '#{email}', '#{username}' ) RETURNING id, name, password, email, username;")
-    User.new(id: result[0]['id'], name: result[0]['name'], password: result[0]['password'], email: result[0]['email'], username: result[0]['username'])
-  end
-
 end
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,5 +1,15 @@
 require 'user'
 
 describe User do
+  
+  describe '.create' do
+    it 'creates a new user instance' do
+      user = User.create(name: "Mr. Blobby", password: "noeledmunds", email: "mrblobby@houseparty.co.uk", username: "mrblobby")
 
+      expect(user).to be_a User
+      expect(user.name).to eq "Mr. Blobby"
+      expect(user.email).to eq "mrblobby@houseparty.co.uk"
+      expect(user.username).to eq("mrblobby")
+    end
+  end
 end
