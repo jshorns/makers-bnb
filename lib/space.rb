7042ce00fdd5d98e3c7d6fe6a4654bf3b9c907ce
Maximilian Clarke <maximilian-jc@live.com>
2021-03-02 15:25:47 +0000

    more dbconnection updates and started space unit tests

--- lib/space.rb
+++ lib/space.rb
@@ -13,21 +13,19 @@ class Space
   def self.create(name:, description:, price:, user_id:)
     if ENV['ENVIRONMENT'] == 'test'
       DBConnection.setup('makersbnb_test')
-      # connection = PG.connect(dbname: 'makersbnb_test')
     else
       DBConnection.setup('makersbnb')
-      # connection = PG.connect(dbname: 'makersbnb')
     end
-
     DBConnection.query("INSERT INTO spaces (name, description, price, user_id) VALUES('#{name}', '#{description}', '#{price}', '#{user_id}' ) RETURNING id, name, description, price;")
-    # result = connection.exec("INSERT INTO spaces (name, description, price, user_id) VALUES('#{name}', '#{description}', '#{price}', '#{user_id}' ) RETURNING id, name, description, price;")
-
-    end
-
+  end
 
   def self.all
-    connection = PG.connect(dbname: 'makersbnb_test')
-    result = connection.exec('SELECT * FROM spaces')
+    if ENV['ENVIRONMENT'] == 'test'
+      DBConnection.setup('makersbnb_test')
+    else
+      DBConnection.setup('makersbnb')
+    end
+    result = DBConnection.query('SELECT * FROM spaces')
     result.reverse_each.map do |space|
       Space.new(name: space['name'], description: space['description'], price: space['price'])
     end
--- spec/unit/space_spec.rb
+++ spec/unit/space_spec.rb
@@ -1,5 +1,20 @@
 require 'space'
 
 describe Space do
+  describe '.create' do
+    it 'creates a new space' do
+      space = Space.create(name: '6 Bedroom Penthouse', description: 'In the heart of London', price: 150, user_id: 1)
+      # persisted_data = persisted_data(table: :users, id: user.id)
+      expect(space).to be_a Space
+      expect(space.name).to eq '6 Bedroom Penthouse'
+      expect(space.description).to eq 'In the heart of London'
+      expect(space.price).to eq 150
+    end
+  end
 
+  describe '.all' do
+    it 'displays all spaces' do
+
+    end
+  end
 end
