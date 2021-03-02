0c4042366f0a37cb0b793b430986ec1415fc4fff
rhc07 <rorieclarke@gmail.com>
2021-03-02 16:23:25 +0000

    Encrypted password and refactored

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,26 @@
 require 'pg'
 require 'db_connection'
+require 'bcrypt'
 
 class User
   def self.create(name:, password:, email:, username:)
-    result = DBConnection.query("INSERT INTO users (name, password, email, username) VALUES('#{name}', '#{password}', '#{email}', '#{username}' ) RETURNING id, name, password, email, username;")
-    User.new(id: result[0]['id'], name: result[0]['name'], password: result[0]['password'], email: result[0]['email'], username: result[0]['username'])
+    encrypted_password = BCrypt::Password.create(password)
+
+    result = DBConnection.query(
+      "INSERT INTO users (name, password, email, username)
+      VALUES('#{name}', '#{encrypted_password}', '#{email}', '#{username}' )
+      RETURNING id, name, password, email, username;"
+    )
+
+    User.new(
+      id:       result[0]['id'],
+      name:     result[0]['name'],
+      password: result[0]['password'],
+      email:    result[0]['email'],
+      username: result[0]['username']
+    )
   end
-  
+
   attr_reader :name, :email, :username, :id
 
   def initialize(name:, password:, email:, username:, id:)
