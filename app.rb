ef6c87c52310185b10d9c67a8e0b1eb432089f2a
Maximilian Clarke <maximilian-jc@live.com>
2021-03-02 11:37:27 +0000

    Updated Space.rb, added spaces.erb, add /spaces route to app.rb TODO; tests

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
+require './lib/space.rb'
 
 class MakersBNB < Sinatra::Base
 
   get '/' do
     erb(:index)
-    #test
+  end
+
+  get '/spaces' do
+    @spaces = Space.all
+    erb(:spaces)
   end
 
   run! if app_file == $0
