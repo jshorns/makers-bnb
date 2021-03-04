b62bf9e34fa0515bda64ee11b0e8d00b06b7dba3
rhc07 <rorieclarke@gmail.com>
2021-03-04 12:00:13 +0000

    added space find

--- lib/space.rb
+++ lib/space.rb
@@ -23,4 +23,17 @@ class Space
       Space.new(id: space['id'], name: space['name'], description: space['description'], price: space['price'], user_id: space['user_id'])
     end
   end
+
+  def self.find(name: nil)
+    return nil unless name
+
+    result = DBConnection.query("SELECT id, name, description, price, user_id FROM spaces WHERE name= '#{name}';")
+    Space.new(
+      id: result[0]['id'],
+      name: result[0]['name'],
+      description: result[0]['description'],
+      price: result[0]['price'],
+      user_id: result[0]['user_id']
+    )
+  end
 end
