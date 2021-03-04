1ebaca2b845b6d7ddd1380d2073616a17307cbc1
Joey <joey.s.hornsby@gmail.com>
2021-03-04 15:34:09 +0000

    added calendar objects and spec

--- spec/unit/spacedate_spec.rb
+++ spec/unit/spacedate_spec.rb
@@ -4,23 +4,23 @@ describe SpaceDate do
   describe '.create' do
     it 'creates a new date' do
       user = User.create(
-        name: "landlord", 
-        password: "123", 
-        email: "email@email.com", 
+        name: "landlord",
+        password: "123",
+        email: "email@email.com",
         username:"rich"
       )
       space = Space.create(
-        name: '6 Bedroom Penthouse', 
-        description: 'In the heart of London', 
-        price: 150, 
+        name: '6 Bedroom Penthouse',
+        description: 'In the heart of London',
+        price: 150,
         user_id: user.id
       )
       date = SpaceDate.create(
-        date: "2021-03-08", 
-        available: true, 
+        date: "2021-03-08",
+        available: true,
         space_id: space.id
       )
-      
+
       expect(date).to be_a SpaceDate
       expect(date.date).to eq "2021-03-08"
       expect(date.available?).to be true
@@ -28,10 +28,4 @@ describe SpaceDate do
     end
   end
 
-
-
-
-  # describe 'date'
-
-  # describe '#available?'
 end
